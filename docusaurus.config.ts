eca9dc4cb47258aa896956d9e53df6954633cab6
osqazi <107077552+osqazi@users.noreply.github.com>
2025-12-05 03:48:05 +0500

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://osqazi.github.io/book_hackathon/humanoid-robotics-book/docs/intro',
+  url: 'https://osqazi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/book_hackathon/',
+  baseUrl: '/book_hackathon/humanoid-robotics-book/docs/intro/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -117,7 +117,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/yourusername/humanoid-robotics-book',
+              href: 'https://github.com/osqazi/book_hackathon/',
             },
           ],
         },
